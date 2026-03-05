84a97b496a4ba76c310821d6e74058bcaa34fcb7
Nguyen It <nguyenhuycuong174@gmail.com>
2026-03-05 08:48:25 +0700

    products

--- client/app/layouts/default.vue
+++ client/app/layouts/default.vue
@@ -1,8 +1,15 @@
+<style scoped>
+.header-top {
+  height: 70px;
+}
+</style>
 <template>
   <div
     class="font-sans antialiased text-slate-700 dark:text-slate-200 bg-pastel-gradient min-h-screen selection:bg-secondary selection:text-white transition-colors duration-300"
   >
-    <LayoutAppHeader />
+    <div class="header-top">
+      <LayoutAppHeader />
+    </div>
     <slot />
     <LayoutAppFooter />
   </div>
