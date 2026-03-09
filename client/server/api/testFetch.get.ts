894b541be61825a1e6f26c13a48f06c4b4847a59
ocean22092004 <vthadaiduong@gmail.com>
2026-03-09 15:51:11 +0700

    refactor struct

---
+++ client/server/api/testFetch.get.ts
@@ -0,0 +1,18 @@
+import { apiFetch } from '~~/server/utils/apiFetch'
+
+export default defineEventHandler(async (event) => {
+  try {
+    const data = await apiFetch('/pages/home/section/simple-slider', {
+      query: { locale: 'vi' }
+    })
+    return { success: true, data }
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+      url: error.request || error.url,
+      status: error.status,
+      data: error.data
+    }
+  }
+})
