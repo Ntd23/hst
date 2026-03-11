e80738245fe5c2d3b60021368e2d1b24d0f02e1e
Nguyen It <nguyenhuycuong174@gmail.com>
2026-03-11 08:31:48 +0700

    Blogs

--- client/app/stores/blog.ts
+++ client/app/stores/blog.ts
@@ -9,11 +9,9 @@ export const useBlogStore = defineStore('blog', {
     actions: {
         async fetchPosts() {
             this.loading = true
-
             try {
                 const data = await $fetch('/api/pages/blog')
                 this.posts = data as any[]
-
             } catch (error) {
                 console.error(error)
             } finally {
