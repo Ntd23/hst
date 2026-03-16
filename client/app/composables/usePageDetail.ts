b67e63e889132ba681f19a153f52c474994ce03a
ocean22092004 <vthadaiduong@gmail.com>
2026-03-16 08:42:53 +0700

    page detail

---
+++ client/app/composables/usePageDetail.ts
@@ -0,0 +1,13 @@
+/**
+ * Reusable composable to fetch all page sections.
+ * Usage: const { data } = await usePageSections('home')
+ */
+export function usePageDetail<T = any>(pageSlug: string) {
+  const { locale } = useI18n()
+  console.log(pageSlug)
+  return useFetch<T>(`/api/pages/${pageSlug}/details`, {
+    key: `details-${pageSlug}-${locale.value}`,
+    query: computed(() => ({ locale: locale.value })),
+    transform: (res: any) => res?.data ?? res,
+  })
+}
